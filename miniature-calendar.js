29b80f881b92084a800c4b0b212b4eecfc184fa5
Wouter Landuydt <wouter.landuydt@gmail.com>
2020-10-18 20:13:05 +0200

    cleanup

--- miniature-calendar.js
+++ miniature-calendar.js
@@ -4,21 +4,20 @@
 
 let w = new ListWidget()
 
-// https://miniature-calendar.com/wp-content/uploads/2020/10/201018sun-768x768.jpg
-
 let today = new Date()
 
 const LOCALE = "en-US"
+const intOpts = { minimumIntegerDigits: 2, useGrouping: false }
 
 let year = today.getFullYear()
-let month = (today.getMonth() + 1).toLocaleString(LOCALE, { minimumIntegerDigits: 2, useGrouping: false })
-let day = today.getDate().toLocaleString(LOCALE, { minimumIntegerDigits: 2, useGrouping: false })
+let month = (today.getMonth() + 1).toLocaleString(LOCALE, intOpts)
+let day = today.getDate().toLocaleString(LOCALE, intOpts)
 let dayName = new Intl.DateTimeFormat(LOCALE, { weekday: 'short' }).format(today).toLowerCase()
-let dimension = 768
-let query = `${year}/${month}/${year.toString().slice(2, 4)}${month}${day}${dayName}-${768}x${768}.jpg`
+let size = 768
+let query = `${year}/${month}/${year.toString().slice(2, 4)}${month}${day}${dayName}-${size}x${size}.jpg`
 
+// example image url: https://miniature-calendar.com/wp-content/uploads/2020/10/201018sun-768x768.jpg
 let url = `https://miniature-calendar.com/wp-content/uploads/${query}`
-console.log(url)
 
 let imageReq = new Request(url)
 let image = await imageReq.loadImage()
