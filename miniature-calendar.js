467e15b71f36edfe65eab87e319016cd07813785
Wouter Landuydt <wouter.landuydt@gmail.com>
2020-10-19 09:21:31 +0200

    updated miniature calendar to support days with multiple images

--- miniature-calendar.js
+++ miniature-calendar.js
@@ -1,29 +1,43 @@
 // Variables used by Scriptable.
 // These must be at the very top of the file. Do not edit.
 // icon-color: blue; icon-glyph: calendar-alt;
-
-let w = new ListWidget()
-
-let today = new Date()
-
 const LOCALE = "en-US"
 const intOpts = { minimumIntegerDigits: 2, useGrouping: false }
+  
+let w = new ListWidget()
 
-let year = today.getFullYear()
-let month = (today.getMonth() + 1).toLocaleString(LOCALE, intOpts)
-let day = today.getDate().toLocaleString(LOCALE, intOpts)
-let dayName = new Intl.DateTimeFormat(LOCALE, { weekday: 'short' }).format(today).toLowerCase()
-let size = 768
-let query = `${year}/${month}/${year.toString().slice(2, 4)}${month}${day}${dayName}-${size}x${size}.jpg`
+const initialImageUrl = getImageUrl();
+let image = await getImage(initialImageUrl);
 
-// example image url: https://miniature-calendar.com/wp-content/uploads/2020/10/201018sun-768x768.jpg
-let url = `https://miniature-calendar.com/wp-content/uploads/${query}`
+if(image === null) {
+  const imageUrl = getImageUrl({tryMultiple: true});
+  image = await getImage(imageUrl);
+}
 
-let imageReq = new Request(url)
-let image = await imageReq.loadImage()
 w.backgroundImage = image
 
 await w.presentSmall()
 
 Script.setWidget(w);
 Script.complete();
+
+function getImageUrl(opts) {
+  const { tryMultiple } = opts || {}
+  let today = new Date()
+  let year = today.getFullYear()
+  let month = (today.getMonth() + 1).toLocaleString(LOCALE, intOpts)
+  let day = today.getDate().toLocaleString(LOCALE, intOpts)
+  let dayName = new Intl.DateTimeFormat(LOCALE, { weekday: 'short' }).format(today).toLowerCase()
+  let size = 768
+  let query = `${year}/${month}/${year.toString().slice(2, 4)}${month}${day}${dayName + (tryMultiple ? "1" : "")}-${size}x${size}.jpg`
+  
+  // example image url: https://miniature-calendar.com/wp-content/uploads/2020/10/201018sun-768x768.jpg
+  let url = `https://miniature-calendar.com/wp-content/uploads/${query}`
+  return url
+}
+
+async function getImage(url) {
+  let imageReq = new Request(url)
+  let image = await imageReq.loadImage().catch(() => null)
+  return image
+}
